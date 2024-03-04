55c3e75d80ddba51b26f34fd14916247701083a9
Umar Murtaza <138823435+Umar21061@users.noreply.github.com>
2024-03-04 11:55:34 +0500

    chng

--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 # user/views.py
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
